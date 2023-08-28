eac500f6701ab76d87d5fde76c0879f4cb731560
krittee-tevamit <ktevamit@gmail.com>
2023-08-28 18:13:07 +0700

    Create WeSplit Form

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var isAmountFocused: Bool
+    
+    var currency: FloatingPointFormatStyle<Double>.Currency = .currency(code: Locale.current.currency?.identifier ?? "USD")
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage / 100)
+        
+        let tipValue = checkAmount * tipSelection
+        let grandToTal = checkAmount + tipValue
+        let amountPerPerson = grandToTal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                // amount and number of people
+                Section {
+                    TextField("Amount", value: $checkAmount, format: currency)
+                        .keyboardType(.decimalPad)
+                        .focused($isAmountFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                // tip percentage
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0..<101, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                // amount per person
+                Section {
+                    Text(0, format: currency)
+                } header: {
+                    Text("Amount per person")
+                }
+                
+                // total amount
+                Section {
+                    Text(0,format: currency)
+                } header: {
+                    Text("Total Amount")
+                }
+                
+            }
+            .navigationTitle("WeSplit")
         }
-        .padding()
     }
 }
 
